dd9180dd2f8f86d2daf400e951946fb17d8794c0
Anderson Paulino <andspaulino@gmail.com>
2020-09-03 14:42:34 -0300

    Completed

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -4,247 +4,162 @@ import java.util.Scanner;
 
 public class Main {
 
-    final static Scanner scanner = new Scanner(System.in);
-    final static int rows = 3;
-    final static int col = 3;
-
     public static void main(String[] args) {
-        char[][] ticTacToe = new char[rows][col];
-        fillTicTacToe(ticTacToe);
-        printTicTacToe(ticTacToe);
-        playGame(ticTacToe);
-    }
+        TicTacToe game = new TicTacToe();
 
-    public static void fillTicTacToe(char[][] ticTacToe) {
+        int moveCount = 0;
+
+        game.start();
+
+        while (!game.validWin() || !game.setDraw(moveCount)) {
+
+            if (game.isMoveValid()) {
+
+                game.printMove(moveCount);
+                ++moveCount;
+            }
+
+            if (game.validWin()) {
+                break;
+            } else if (game.setDraw(moveCount)) {
+                break;
+            }
+        }
+    }
+}
+
+class TicTacToe {
+
+    public Scanner scanner = new Scanner(System.in);
+    public int rows = 3;
+    public int col = 3;
+    public char[][] matrix = new char[rows][col];
+    public int xCordReal, yCordReal;
+
+
+    public void start() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < col; j++) {
-                ticTacToe[i][j] = ' ';
+                matrix[i][j] = ' ';
             }
         }
+        printMatrix();
     }
 
-    public static void playGame(char[][] ticTacToe) {
-        playerOneTicTacToe(ticTacToe);
-        playerTwoTicTacToe(ticTacToe);
-        //isFull(ticTacToe);
-    }
-
-    public static void playerOneTicTacToe(char[][] ticTacToe){
-        boolean validMove = false;
-        int xCordReal, yCordReal;
-        int xCord, yCord;
-
-        while (!validMove) {
-            System.out.print("Enter the coordinates: ");
-            if (scanner.hasNextInt()) {
-                xCord = scanner.nextInt();
-            } else {
-                scanner.nextLine();
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            if (scanner.hasNextInt()) {
-                yCord= scanner.nextInt();
-            } else {
-                scanner.nextLine();
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            xCordReal = 3 - yCord;
-            yCordReal = xCord - 1;
-            if (xCordReal < 0 || xCordReal > 2 || yCordReal < 0 || yCordReal > 2) {
-                System.out.println("Coordinates should be from 1 to 3!");
-                continue;
-            }
-            if (ticTacToe[xCordReal][yCordReal] != ' ') {
-                System.out.println("This cell is occupied! Choose another one!");
-                continue;
-            }
-            validMove = true;
-
-            ticTacToe[xCordReal][yCordReal] = 'X';
-            printTicTacToe(ticTacToe);
-            resultChecker(ticTacToe);
-        }
-    }
-
-    public static void playerTwoTicTacToe(char[][] ticTacToe){
-        boolean validMove = false;
-        int xCordReal, yCordReal;
-        int xCord, yCord;
-
-        while (!validMove) {
-            System.out.print("Enter the coordinates: ");
-            if (scanner.hasNextInt()) {
-                xCord = scanner.nextInt();
-            } else {
-                scanner.nextLine();
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            if (scanner.hasNextInt()) {
-                yCord= scanner.nextInt();
-            } else {
-                scanner.nextLine();
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            xCordReal = 3 - yCord;
-            yCordReal = xCord - 1;
-            if (xCordReal < 0 || xCordReal > 2 || yCordReal < 0 || yCordReal > 2) {
-                System.out.println("Coordinates should be from 1 to 3!");
-                continue;
-            }
-            if (ticTacToe[xCordReal][yCordReal] != ' ') {
-                System.out.println("This cell is occupied! Choose another one!");
-                continue;
-            }
-            validMove = true;
-
-            ticTacToe[xCordReal][yCordReal] = 'O';
-            printTicTacToe(ticTacToe);
-            resultChecker(ticTacToe);
-        }
-    }
-
-    public static int isFull(char[][] ticTacToe) {
-        int counter = 0;
-        for (char[] whiteSpaceFinderOne : ticTacToe) {
-            for (char whiteSpaceFinderTwo : whiteSpaceFinderOne) {
-                if (whiteSpaceFinderTwo == ' ') {
-                    counter++;
-                }
-            }
-        }
-        return counter;
-    }
-
-    public static void resultChecker(char[][] ticTacToe) {
-
-        boolean impossible = false;
-        boolean draw = false;
-        boolean xWins = false;
-        boolean oWins = false;
-        boolean whiteSpace = false;
-
-
-        int rowWinner;
-        for (int i = 0; i < rows; i++) {
-            rowWinner = ticTacToe[i][0] + ticTacToe[i][1] + ticTacToe[i][2];
-            if (rowWinner == 264 || rowWinner == 237) {
-                if (rowWinner == 264) {
-                    xWins = true;
-                } else {
-                    oWins = true;
-                }
-            }
-        }
-
-        int colWinner;
-        for (int i = 0; i < rows; i++) {
-            colWinner = ticTacToe[0][i] + ticTacToe[1][i] + ticTacToe[2][i];
-            if (colWinner == 264 || colWinner == 237) {
-                if (colWinner == 264) {
-                    xWins = true;
-                } else {
-                    oWins = true;
-                }
-            }
-
-        }
-
-        int diagDownwardWinner = ticTacToe[0][0] + ticTacToe[1][1] + ticTacToe[2][2];
-        if (diagDownwardWinner == 264 || diagDownwardWinner == 237) {
-            if (diagDownwardWinner == 264) {
-                xWins = true;
-            } else {
-                oWins = true;
-            }
-        }
-
-        int diagUpwardWinner = ticTacToe[2][0] + ticTacToe[1][1] + ticTacToe[0][2];
-        if (diagUpwardWinner == 264 || diagUpwardWinner == 237) {
-            if (diagUpwardWinner == 264) {
-                xWins = true;
-            } else {
-                oWins = true;
-            }
-        }
-
-
-        int xFinderCount = 0;
-        for (char[] xFinders : ticTacToe) {
-            for (char xFinder : xFinders) {
-                if (xFinder == 'X') {
-                    xFinderCount++;
-                }
-            }
-        }
-
-        int oFinderCount = 0;
-        for (char[] oFinders : ticTacToe) {
-            for (char oFinder : oFinders) {
-                if (oFinder == 'O') {
-                    oFinderCount++;
-                }
-            }
-        }
-
-        for (char[] whiteSpaceFinderOne : ticTacToe) {
-            for (char whiteSpaceFinderTwo : whiteSpaceFinderOne) {
-                if (whiteSpaceFinderTwo == ' ') {
-                    whiteSpace = true;
-                    break;
-                }
-            }
-        }
-
-        if (xFinderCount - oFinderCount > 1) {
-            impossible = true;
-        } else if (xWins && oWins) {
-            impossible = true;
-        }
-
-        if (impossible) {
-            System.out.println("Impossible");
-        }
-        if (!xWins && !oWins) {
-            draw = true;
-        }
-
-        if(draw && !impossible) {
-            if (whiteSpace) {
-                System.out.println("Game not finished");
-                playGame(ticTacToe);
-            }
-        }
-
-        if (draw && !whiteSpace) {
-            System.out.println("Draw");
-        }
-
-        if (xWins && !impossible) {
-            System.out.println("X wins");
-        }
-
-        if (oWins && !impossible) {
-            System.out.println("O wins");
-        }
-
-
-
-    }
-
-    public static void printTicTacToe(char[][] ticTacToe) {
+    public void printMatrix() {
         System.out.println("---------");
         for (int i = 0; i < rows; i++) {
             System.out.print("| ");
             for (int j = 0; j < col; j++) {
-                System.out.print(ticTacToe[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.print("|");
             System.out.println();
         }
         System.out.println("---------");
+    }
+
+    public boolean isMoveValid() {
+        boolean validMove = false;
+        int xCord, yCord;
+
+        while (!validMove) {
+            System.out.print("Enter the coordinates: ");
+            if (scanner.hasNextInt()) {
+                xCord = scanner.nextInt();
+            } else {
+                scanner.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+            if (scanner.hasNextInt()) {
+                yCord = scanner.nextInt();
+            } else {
+                scanner.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+            xCordReal = 3 - yCord;
+            yCordReal = xCord - 1;
+            if (xCordReal < 0 || xCordReal > 2 || yCordReal < 0 || yCordReal > 2) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            if (matrix[xCordReal][yCordReal] != ' ') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+            validMove = true;
+
+        }
+        return true;
+    }
+
+    public void printMove(int moveCount) {
+        if (moveCount % 2 == 0) {
+            matrix[xCordReal][yCordReal] = 'X';
+        } else {
+            matrix[xCordReal][yCordReal] = 'O';
+        }
+        printMatrix();
+    }
+
+    public boolean validWin() {
+        boolean winner = false;
+        String oWins = "O wins";
+        String xWins = "X wins";
+
+
+        for (int i = 0; i < rows; i++) {
+            int rowWinner = matrix[i][0] + matrix[i][1] + matrix[i][2];
+            if (rowWinner == 264) {
+                System.out.println(xWins);
+                winner = true;
+            } else if (rowWinner == 237) {
+                System.out.println(oWins);
+                winner = true;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            int colWinner = matrix[0][i] + matrix[1][i] + matrix[2][i];
+            if (colWinner == 264) {
+                System.out.println(xWins);
+                winner = true;
+            } else if (colWinner == 237) {
+                System.out.println(oWins);
+                winner = true;
+            }
+
+        }
+
+        int diagDownwardWinner = matrix[0][0] + matrix[1][1] + matrix[2][2];
+        if (diagDownwardWinner == 264) {
+            System.out.println(xWins);
+            winner = true;
+        } else if (diagDownwardWinner == 237) {
+            System.out.println(oWins);
+            winner = true;
+        }
+
+
+        int diagUpwardWinner = matrix[2][0] + matrix[1][1] + matrix[0][2];
+        if (diagUpwardWinner == 264) {
+            System.out.println(xWins);
+            winner = true;
+        } else if (diagUpwardWinner == 237) {
+            System.out.println(oWins);
+            winner = true;
+        }
+        return winner;
+    }
+    public boolean setDraw(int moveCount) {
+        boolean draw = false;
+
+        if (!validWin() && moveCount == 9) {
+            System.out.println("Draw");
+            draw = true;
+        }
+        return draw;
     }
 }
